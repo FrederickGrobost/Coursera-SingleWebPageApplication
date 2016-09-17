0281c32756e6c1940e8134c6b3879f9ef0495ef8
Frederick GROBOST <fgrobost@amadeus.com>
2016-09-17 23:26:40 +0200

    Assignment 1 completed

---
+++ module1-solution/app.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+  $scope.evaluateLunch = function ()
+  {
+    $scope.result = "";
+
+    if($scope.listOfLunch)
+    {
+
+      var splittedLunch = ($scope.listOfLunch).split(',');
+      if(splittedLunch.length <= 3)
+      {
+        $scope.result = 'it is Ok';
+      } else {
+        $scope.result = 'Too much';
+      }
+    } else {
+      $scope.result = "Please enter data first";
+    }
+  };
+
+}
+
+})();
